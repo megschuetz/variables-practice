1be7449f5ae9d7a1a446919b2d9bd886d923b083
Megan Schuetz <mschuetz411@gmail.com>
2022-02-24 20:18:02 -0700

    Add annotation and boolean

--- variables.js
+++ variables.js
@@ -3,7 +3,17 @@ var petName = 'Nelly';
 var bestFriend ='Lauren';
 var sister = 'Katie';
 
+console.log(name, petName,bestFriend,sister);
 
 var numberOfDogs = 1;
 var dogAge = 4
 var costOfDog = 400
+
+console.log(numberOfDogs,dogAge,costOfDog);
+
+// variable defined as frontEnd assigned true
+var frontEnd = true
+var march21 = true
+var quitJob = false
+
+console.log(frontEnd,march21,quitJob);
